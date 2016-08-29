15b37282daacb555f760204cb0c671a8a2c7ee74
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-29 16:46:27 +0530

    session1assignment2

---
+++ src/variabledeclaration/variable.java
@@ -0,0 +1,14 @@
+package variabledeclaration;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+b=4;
+a=2;
+c=5;
+System.out.println(a+b+c);
+	}
+
+}
